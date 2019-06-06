a56365cb8009ed955f998e69761273ce7ac55aa3
René <rene@sq4.nl>
2019-06-06 11:55:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var electron = require('electron') // http://electron.atom.io/docs/api
+var path = require('path') // https://nodejs.org/api/path.html
+var url = require('url') // https://nodejs.org/api/url.html
+var window = null
+const { exec } = require('child_process');
+
+electron.app.setPath('userData', __dirname + '/data');
+
+electron.app.once('ready', function () {
+    window = new electron.BrowserWindow({
+        allowRunningInsecureContent: true,
+        width: 1920,
+        height: 1080,
+        backgroundColor: "#000000",
+        show: false,
+        frame: false
+    });
+
+    window.loadURL('file://' + __dirname + '/index.html');
+
+    window.on('closed', function () {
+        window = null;
+        electron.app.quit();
+    });
+
+    window.once('ready-to-show', function () {
+        window.setMenuBarVisibility(false);
+        window.show()
+	exec("sudo killall -9 omxplayer.bin");
+    })
+})
